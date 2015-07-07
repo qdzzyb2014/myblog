0b10465e105c3654d8cb380c2b0cd6334c9240a3
隔壁小王 <qdzzyb2015@gmail.com>
2015-07-07 21:00:35 +0800

    加入bootstrap

--- app/form.py
+++ app/form.py
@@ -4,11 +4,13 @@ from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import Required
 
+
 class LoginForm(Form):
     user_name = TextField('user_name', validators = [Required()])
     password = PasswordField('password', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
     submit = SubmitField('登陆'.decode('utf-8'))
+
 
 class EditForm(Form):
     title = TextField('title', validators = [Required()])
